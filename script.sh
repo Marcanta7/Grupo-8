ef21582fc4b909f6064f98395fb29f12ab201f90
Joel Seguí Far <joelsf43@gmail.com>
2024-10-07 19:25:15 +0200

    Update

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@
 read -p "¿Cómo te llamas? " nombre
 echo $nombre > nombre.txt
 mkdir PARAMETRO
-date +"%M" > /PARAMETRO/ganador.txt
+
+date +"%M" > PARAMETRO/GANADOR.txt
 
 if (( minutos % 2 == 0 )); then
     # Si los minutos son pares, agregar el mensaje de que ha ganado
